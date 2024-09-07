cfe1f0c1fdd1a21124c0531d692f3b2cc91806c0
Apicha660612162 <apichalerdjintanakid@gmail.com>
2024-09-07 14:36:47 +0700

    gg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { Container, Text, Title } from "@mantine/core";
-
+import YourRating from "@components/YourRating";
 export default function Home() {
   return (
     <Container size="600px">
-      <Title order={2}>Food Review 🍕</Title>
-
-      <Text ta="center" my="sm">
-        Copyright © 2024 Chayanin Suatap 650610560
+      <Title order={2} >Food Review 🍕</Title>
+      <YourRating/>
+    
+      <Text ta="center" my="sm" c="dimmed">
+        Copyright © 2024 Apicha LErdjintanakit 660612162
       </Text>
     </Container>
   );
